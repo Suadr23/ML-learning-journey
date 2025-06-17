83a33f83bed289fe75082cd2570a01ebe3e19470
Suaad R <suaadalhashimi14@gmail.com>
2025-06-17 14:47:01 +0400

    Add initial ML classification code

---
+++ Linear and logistic Regression.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.datasets import make_classification
+
+X, y = make_classification(n_samples=100, n_features=2, n_informative=1, random_state=42)
+
+clf = LogisticRegression(C=250)
+clf.fit(X, y)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+x_test = np.linspace(X[:, 0].min(), X[:, 0].max(), 300).reshape(-1, 1)
+
+x_test_full = np.hstack([x_test, np.full_like(x_test, X[:,1].mean())])
+
+y_prob = clf.predict_proba(x_test_full)[:, 1]
+
+y_lin = lin_reg.predict(x_test_full)
+
+plt.scatter(X[:, 0], y, c=y, cmap='bwr', alpha=0.5)
+
+plt.plot(x_test, y_lin, color='green', label='Linear Regression')
+
+plt.plot(x_test, y_prob, color='black', label='Logistic Regression Probability')
+
+plt.xlabel('Feature 1')
+plt.ylabel('Target / Probability')
+plt.legend()
+plt.title('Logistic vs Linear Regression on 1 Feature')
+plt.show()
--- import pandas as pd.py
+++ import pandas as pd.py
@@ -2,23 +2,19 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-# تحميل البيانات
-data = pd.read_excel(r'C:\Users\USER PC\OneDrive\Desktop\Machine Learning\house_prices.xlsx')
+data = pd.read_excel('house_prices.xlsx')  
 X = data[['Size']]
 y = data['Price']
 
-# تدريب النموذج
 model = LinearRegression()
 model.fit(X, y)
 
-# التنبؤ
 predictions = model.predict(X)
 
-# الرسم البياني
 plt.scatter(X, y, color='green', label='Actual Prices')
 plt.plot(X, predictions, color='orange', label='Predicted Prices')
 plt.xlabel('House Size (sqm)')
-plt.ylabel('Price (SAR)')
+plt.ylabel('Price (OMR)')
 plt.title('House Size vs Price')
 plt.legend()
-plt.show()
+plt.show() 
